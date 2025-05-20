b942eb46bb47d320aecae68a73ef0f481cc4a2de
Aphiwe Mhotwana <127925737+ST10085670@users.noreply.github.com>
2025-05-20 08:51:36 +0200

    New Lecturer file

---
+++ WilPWA/Controllers/LectureController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WilPWA.Controllers
+{
+    public class LectureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Card()
+        {
+            return View();
+        }
+        public IActionResult Progress()
+        {
+            return View();
+        }
+        public IActionResult Report()
+        {
+            return View();
+        }
+    }
+}
