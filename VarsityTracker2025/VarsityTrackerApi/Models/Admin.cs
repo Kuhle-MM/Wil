5cb1accad71b4f34ad86f83bfb62c56bf7eef43e
KMBotha <st10258494@vcconnect.edu.za>
2025-05-21 14:01:27 +0200

    Added new solution with blank mvc and api

---
+++ VarsityTracker2025/VarsityTrackerApi/Models/Admin.cs
@@ -0,0 +1,22 @@
+﻿using Azure;
+using System.ComponentModel.DataAnnotations;
+
+namespace VarsityTrackerApi.Models
+{
+    public class Admin
+    {
+        [Key]
+        public string adminID { get; set; }
+        public string? adminEmail { get; set; }
+        public string? phoneNumber { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string? password { get; set; }
+
+        //ITableEntity implementation
+        public string? PartitionKey { get; set; }
+        public string? RowKey { get; set; }
+        public ETag ETag { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+    }
+}
---
+++ VarsityTracker2025/VarsityTrackerApi/Models/Students.cs
@@ -0,0 +1,24 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System.ComponentModel.DataAnnotations;
+
+namespace VarsityTrackerApi.Models
+{
+    public class Students : ITableEntity
+    {
+        [Key]
+        public string studentNumber { get; set; }
+        public string? studentEmail { get; set; }
+        public string? phoneNumber { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string? password { get; set; }
+        public string? role { get; set; }
+
+        //ITableEntity implementation
+        public string? PartitionKey { get; set; }
+        public string? RowKey { get; set; }
+        public ETag ETag { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+    }
+}
