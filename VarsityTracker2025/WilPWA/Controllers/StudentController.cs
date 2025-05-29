b2f24fd2dda422d61f8a92e84b4f8af6b0357834
KMBotha <st10258494@vcconnect.edu.za>
2025-05-29 10:18:41 +0200

    Replaced MVC with correct project

---
+++ VarsityTracker2025/WilPWA/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WilPWA.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult Report()
+        {
+            return View();
+        }
+        public IActionResult SetCalendar()
+        {
+            return View();
+        }
+        
+    }
+}
+
+//Welcome page
+
