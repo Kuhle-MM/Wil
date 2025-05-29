ab7c76fbd7c866314c18ef3cbd7ba86368d25a21
KMBotha <st10258494@vcconnect.edu.za>
2025-05-29 09:50:06 +0200

    Deleted unused files

--- Backend - Kelly/prjBackendApi/Models/Login.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace prjBackendApi.Models
-{
-    public class Login
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
--- WilPWA/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WilPWA.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
