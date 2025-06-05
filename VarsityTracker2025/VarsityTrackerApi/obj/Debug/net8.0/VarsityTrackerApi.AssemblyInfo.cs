90bd2cb2eb764c35e816269ddbe856d6dcab6c7a
ST10284732 <126753862+ST10284732@users.noreply.github.com>
2025-06-05 09:45:38 +0200

    Lecturer clockin/clockout feature added

--- VarsityTracker2025/VarsityTrackerApi/obj/Debug/net8.0/VarsityTrackerApi.AssemblyInfo.cs
+++ VarsityTracker2025/VarsityTrackerApi/obj/Debug/net8.0/VarsityTrackerApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VarsityTrackerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa31cd5c1c07f0865d28f6f062383894dc858f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecd405d65b38d06d0ff24a460891270d24e2230e")]
 [assembly: System.Reflection.AssemblyProductAttribute("VarsityTrackerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VarsityTrackerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
